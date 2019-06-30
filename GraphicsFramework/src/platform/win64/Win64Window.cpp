583fa872de4c02bd34779f3e4de3b136f1e3aa00
Nick De Breuck <nick.de.breuck@outlook.com>
2019-06-30 22:31:23 +0200

    created win64window class

---
+++ GraphicsFramework/src/platform/win64/Win64Window.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+
+#include "Win64Window.h"
+
+Win64Window::Win64Window(unsigned int width, unsigned int height) :
+	Window(width, height)
+{
+}
+
+Win64Window::~Win64Window() = default;
---
+++ GraphicsFramework/src/platform/win64/Win64Window.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "graphics/window.h"
+
+class Win64Window : public Window
+{
+public: 
+	Win64Window(unsigned int width, unsigned int height);
+	~Win64Window();
+
+private:
+
+};
