fddc07f22b64762f1d664341cbc89f94812e6bab
Nick De Breuck <nick.de.breuck@outlook.com>
2019-06-30 22:30:19 +0200

    created pre compiled header

---
+++ GraphicsFramework/src/stdafx.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// spdlog headers
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+
+// stl headers
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+
+// windows headers
+#include <Windows.h>
+#include <windowsx.h>
+
