0e07b63450566c5a92a05431bafdae37150bfed6
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-02 00:11:05 +0200

    index buffer created based on api

--- GraphicsFramework/src/graphics/IndexBuffer.cpp
+++ GraphicsFramework/src/graphics/IndexBuffer.cpp
@@ -2,7 +2,23 @@
 
 #include "IndexBuffer.h"
 
-IndexBuffer::IndexBuffer()
+#include "platform/opengl/OpenGLIndexBuffer.h"
+
+#include "Context.h"
+
+std::unique_ptr<IndexBuffer> IndexBuffer::Create(size_t count)
+{
+	switch (Context::GetCurrent()->GetApiType())
+	{
+	case Context::API::OpenGL:
+		return std::make_unique<OpenGLIndexBuffer>(count);
+	}
+
+	return nullptr;
+}
+
+IndexBuffer::IndexBuffer(size_t count):
+	m_count(count)
 {
 
 }
@@ -10,4 +26,9 @@ IndexBuffer::IndexBuffer()
 IndexBuffer::~IndexBuffer()
 {
 
+}
+
+size_t IndexBuffer::GetCount() const
+{
+	return m_count;	
 }
--- GraphicsFramework/src/graphics/IndexBuffer.h
+++ GraphicsFramework/src/graphics/IndexBuffer.h
@@ -3,6 +3,17 @@
 class IndexBuffer
 {
 public:
-	IndexBuffer();
+	IndexBuffer(size_t count);
 	virtual ~IndexBuffer();
+
+	virtual void SetData(void* data) = 0;
+	virtual void Bind() const = 0;
+	virtual void Unbind() const = 0;
+
+	size_t GetCount() const;
+
+	static std::unique_ptr<IndexBuffer> Create(size_t count);
+
+protected:
+	size_t m_count;
 };
