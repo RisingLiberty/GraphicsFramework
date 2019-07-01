336e551dbc04c64e42aa6d6aaaa1b1ae5432fac2
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-01 11:53:00 +0200

    main window initialization

--- GraphicsFramework/src/graphics/Window.cpp
+++ GraphicsFramework/src/graphics/Window.cpp
@@ -2,10 +2,23 @@
 
 #include "Window.h"
 
-Window::Window(unsigned int width, unsigned int height) :
-	m_width(width),
-	m_height(height)
+Window::Window(unsigned int width, unsigned int height, bool isFullScreen, bool isVSync):
+	m_is_paused(false)
 {
+	m_properties.width = width;
+	m_properties.height = height;
+	m_properties.is_fullscreen = isFullScreen;
+	m_properties.vsync = isVSync;
 }
 
 Window::~Window() = default;
+
+void Window::SetEventCallback(const WindowEventCallback& callback)
+{
+	m_event_callback = callback;
+}
+
+bool Window::IsPaused() const
+{
+	return m_is_paused;
+}
--- GraphicsFramework/src/graphics/Window.h
+++ GraphicsFramework/src/graphics/Window.h
@@ -1,12 +1,33 @@
 #pragma once
 
+#include "Events/event.h"
+
+using WindowEventCallback = std::function<void(const Event& event)>;
+
 class Window
 {
 public:
-	Window(unsigned int width, unsigned int height);
+	Window(unsigned int width, unsigned int height, bool isFullScreen = false, bool isVsync = false);
 	virtual ~Window();
 
-private:
-	unsigned int m_width;
-	unsigned int m_height;
+	virtual int EventLoop() = 0;
+	virtual void* GetHandle() const = 0;
+
+	void SetEventCallback(const WindowEventCallback& callback);
+
+	bool IsPaused() const;
+
+protected:
+	struct Properties
+	{
+		unsigned int width;
+		unsigned int height;
+		bool is_fullscreen;
+		bool vsync;
+	};
+	Properties m_properties;
+
+	WindowEventCallback m_event_callback;
+	bool m_is_paused;
+	
 };
