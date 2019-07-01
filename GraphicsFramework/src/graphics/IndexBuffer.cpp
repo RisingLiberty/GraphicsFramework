2084ff065a9d4636a711fc4fa48ca2a9217a5826
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-01 17:29:48 +0200

    vertex and index buffer class created

---
+++ GraphicsFramework/src/graphics/IndexBuffer.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+
+#include "IndexBuffer.h"
+
+IndexBuffer::IndexBuffer()
+{
+
+}
+
+IndexBuffer::~IndexBuffer()
+{
+
+}
---
+++ GraphicsFramework/src/graphics/VertexBuffer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class VertexBuffer
+{
+public:
+	VertexBuffer(size_t size);
+	virtual ~VertexBuffer();
+
+
+
+private:
+
+};
