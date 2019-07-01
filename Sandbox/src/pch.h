0f2d02ac7655cf9980b4897fc293cbbac1e8233a
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-01 11:56:14 +0200

    sandbox pch added

---
+++ Sandbox/src/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// stl headers
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <memory>
