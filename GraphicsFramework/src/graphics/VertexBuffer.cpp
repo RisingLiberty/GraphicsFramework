671756cdc4a98086ce7fd9758df0ac430a54037b
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-02 00:11:16 +0200

    vertex buffer gets created based on api

--- GraphicsFramework/src/graphics/VertexBuffer.cpp
+++ GraphicsFramework/src/graphics/VertexBuffer.cpp
@@ -1,3 +1,29 @@
 #include "stdafx.h"
 
 #include "VertexBuffer.h"
+#include "platform/opengl/OpenGLVertexBuffer.h"
+
+#include "Context.h"
+
+std::unique_ptr<VertexBuffer> VertexBuffer::Create(size_t size, VertexBuffer::BufferUsage usage)
+{
+	switch (Context::GetCurrent()->GetApiType())
+	{
+	case Context::API::OpenGL:
+		return std::make_unique<OpenGLVertexBuffer>(size, usage);
+	}
+
+	return nullptr;
+}
+
+VertexBuffer::VertexBuffer(size_t size, BufferUsage usage):
+	m_size(size),
+	m_usage(usage)
+{
+
+}
+
+VertexBuffer::~VertexBuffer()
+{
+
+}
--- GraphicsFramework/src/graphics/VertexBuffer.h
+++ GraphicsFramework/src/graphics/VertexBuffer.h
@@ -3,11 +3,23 @@
 class VertexBuffer
 {
 public:
-	VertexBuffer(size_t size);
+	enum class BufferUsage
+	{
+		DYNAMIC,
+		STATIC
+	};
+
+	VertexBuffer(size_t size, BufferUsage usage);
 	virtual ~VertexBuffer();
 
+	virtual void SetData(void* data) = 0;
+	virtual void Bind() const = 0;
+	virtual void Unbind() const = 0;
 
+	static std::unique_ptr<VertexBuffer> Create(size_t size, BufferUsage usage = BufferUsage::STATIC);
 
-private:
+protected:
+	size_t m_size;
+	BufferUsage m_usage;
 
 };
