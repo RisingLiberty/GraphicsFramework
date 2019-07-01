f9cb6afaeed380dd99a8410cc8d669169642c583
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-01 11:52:36 +0200

    application's frame stats now get outputted

--- GraphicsFramework/src/core/Application.cpp
+++ GraphicsFramework/src/core/Application.cpp
@@ -1,6 +1,42 @@
 #include "stdafx.h"
 
 #include "Application.h"
+#include "graphics/window.h"
+#include "Timer.h"
 
-Application::Application() = default;
+namespace
+{
+	const float UPDATE_FREQUENCY = 60;
+	const float SECONDS_PER_UPDATE = 1.0f / UPDATE_FREQUENCY;
+}
+
+Application::Application()
+{
+};
+
 Application::~Application() = default;
+
+void Application::CalculateFrameStats() const
+{
+	// Calculates fps and time it takes to render 1 frame
+	static int frameCount = 0;
+	static float timeElapsed = 0.0f;
+
+	frameCount++;
+
+	// Update every second
+	if ((m_timer->GetEllapsedSeconds() - timeElapsed) >= 1.0f)
+	{
+		float fps = (float)frameCount; //after 1 second, frameCount holds the number of frames that passed.
+		float mspf = 1000.0f / fps;
+
+		// make the logging fixed
+		std::cout << std::fixed;
+
+		spdlog::info("fps: {}", fps);
+		spdlog::info("milisecondss per frame: {}", mspf);
+
+		frameCount = 0;
+		++timeElapsed;
+	}
+}
--- GraphicsFramework/src/core/Application.h
+++ GraphicsFramework/src/core/Application.h
@@ -1,9 +1,26 @@
 #pragma once
 
+class Window;
+class Timer;
+
+#include "Events/Event.h"
+
 class Application
 {
 public:
 	Application();
 	~Application();
 
+	virtual void OnEvent(const Event& event) = 0;
+	virtual void Run() = 0;
+
+protected:
+	void CalculateFrameStats() const;
+
+	virtual void Update(float dTime) = 0;
+	virtual void Draw() = 0;
+
+protected:
+	std::unique_ptr<Window> m_window;
+	std::unique_ptr<Timer> m_timer;
 };
