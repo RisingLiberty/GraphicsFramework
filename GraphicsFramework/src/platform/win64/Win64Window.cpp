4cdeb62551d53c38ccb5fd37992f95b0f6e14952
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-01 11:51:50 +0200

    Initialization code of win64 window for opengl

--- GraphicsFramework/src/platform/win64/Win64Window.cpp
+++ GraphicsFramework/src/platform/win64/Win64Window.cpp
@@ -1,10 +1,137 @@
 #include "stdafx.h"
 
 #include "Win64Window.h"
+#include "DefaultWndClass.h"
 
-Win64Window::Win64Window(unsigned int width, unsigned int height) :
-	Window(width, height)
+namespace
 {
+	PIXELFORMATDESCRIPTOR GetPixelFormat()
+	{
+		PIXELFORMATDESCRIPTOR pfd = {};
+		pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+		pfd.nVersion = 1;
+		pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+		pfd.iPixelType = PFD_TYPE_RGBA;
+		pfd.cColorBits = 32;
+		pfd.cDepthBits = 24;
+		pfd.cStencilBits = 8;
+		pfd.cAuxBuffers = 0;
+		pfd.iLayerType = PFD_MAIN_PLANE;
+		return pfd;
+	}
+}
+
+Win64Window::Win64Window(unsigned int width, unsigned int height, const std::wstring& title) :
+	Window(width, height),
+	m_hinstance(GetModuleHandle(0))
+{
+	const std::wstring window_class_name = L"DirectXWindowClass";
+	DefaultWndClass wnd_class((WNDPROC)WindowProcdureStatic, m_hinstance, window_class_name.c_str());
+
+	RECT r = { 0,0, (LONG)width, (LONG)height };
+	AdjustWindowRect(&r, WS_OVERLAPPED, false);
+	int win_width = r.right - r.left;
+	int win_height = r.bottom - r.top;
+
+	m_handle = CreateWindowExW(
+		0L,
+		window_class_name.c_str(),
+		title.c_str(),
+		WS_OVERLAPPED,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		win_width,
+		win_height,
+		NULL,			//no parent window
+		nullptr,		//not using menus
+		m_hinstance,
+		this
+	);
+
+	if (!m_handle)
+	{
+		DWORD err = GetLastError();
+		ThrowIfFailedWindow(HRESULT_FROM_WIN32(err));
+	}
+
+	m_hdc = GetDC(m_handle);
+	PIXELFORMATDESCRIPTOR pfd = GetPixelFormat();
+	int pixel_format = ChoosePixelFormat(m_hdc, &pfd); // let windows choose pixel format.
+	if (pixel_format)
+	{
+		if (!SetPixelFormat(m_hdc, pixel_format, &pfd))
+			ThrowException(L"Failed to set pixel format!");
+	}
+	else
+		ThrowException(L"Failed to choose pixel format!");
+
+	ShowWindow(m_handle, SW_SHOWDEFAULT);
 }
 
 Win64Window::~Win64Window() = default;
+
+LRESULT Win64Window::WindowProcdureStatic(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	if (message == WM_CREATE)
+	{
+		CREATESTRUCT* pCs = reinterpret_cast<CREATESTRUCT*>(lParam);
+		SetWindowLongPtrW(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pCs->lpCreateParams));
+	}
+	else
+	{
+		Win64Window* pThis = reinterpret_cast<Win64Window*>(GetWindowLongPtrW(hwnd, GWLP_USERDATA));
+		if (pThis)
+			return pThis->HandleEvent(hwnd, message, wParam, lParam);
+	}
+
+	return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+LRESULT Win64Window::HandleEvent(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		//Is sent when the window is being destroyed
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_ACTIVATE:
+		if (wParam == WA_ACTIVE || wParam == WA_CLICKACTIVE)
+			m_is_paused = false;
+		else
+			m_is_paused = true;
+		return 0;
+	case WM_RBUTTONDOWN:
+		//OnMouseDown(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+		return 0;
+	case WM_RBUTTONUP:
+		//OnMouseUp(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+		return 0;
+	case WM_MOUSEMOVE:
+		//OnMouseMove(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+
+	default:
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+}
+
+int Win64Window::EventLoop()
+{
+	MSG message = { 0 };
+
+	while (PeekMessage(&message, 0, 0, 0, PM_REMOVE))
+	{
+		TranslateMessage(&message);
+		DispatchMessage(&message);
+
+		if (message.message == WM_QUIT)
+			return message.message;
+	}
+
+	return message.message;
+}
+
+void* Win64Window::GetHandle() const
+{
+	return m_handle;
+}
--- GraphicsFramework/src/platform/win64/Win64Window.h
+++ GraphicsFramework/src/platform/win64/Win64Window.h
@@ -5,9 +5,16 @@
 class Win64Window : public Window
 {
 public: 
-	Win64Window(unsigned int width, unsigned int height);
-	~Win64Window();
+	Win64Window(unsigned int width, unsigned int height, const std::wstring& title);
+	virtual ~Win64Window();
 
+	static LRESULT CALLBACK WindowProcdureStatic(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+	LRESULT HandleEvent(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+	int EventLoop() override;
+	void* GetHandle() const override;
 private:
-
+	HWND m_handle;
+	HINSTANCE m_hinstance;
+	HDC m_hdc;
 };
