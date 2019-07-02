313a023c5ff3dfd42feeacd936ec7e42cbb1e514
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-02 04:16:20 +0200

    Renderer now renders using SceneObjects

--- GraphicsFramework/src/platform/opengl/OpenGLRenderer.cpp
+++ GraphicsFramework/src/platform/opengl/OpenGLRenderer.cpp
@@ -2,6 +2,9 @@
 
 #include <GL/glew.h>
 #include <graphics/Mesh.h>
+#include <graphics/Material.h>
+
+#include <scenegraph/SceneObject.h>
 
 #include "HelperMethods.h"
 #include "OpenGLRenderer.h"
@@ -15,99 +18,6 @@
 
 #include <graphics/MeshFactory.h>
 
-namespace
-{
-	struct ShaderProgramSource
-	{
-		std::string VertexSource;
-		std::string FragmentSource;
-	};
-
-	static ShaderProgramSource ParseShader(const std::string& filePath)
-	{
-		std::ifstream stream(filePath);
-
-		enum class ShaderType
-		{
-			NONE = -1,
-
-			VERTEX = 0,
-			FRAGMENT = 1
-		};
-
-		if (!stream.is_open())
-			return {};
-
-		std::string line;
-		std::stringstream ss[2];
-		ShaderType type = ShaderType::NONE;
-
-		while (std::getline(stream, line))
-		{
-			if (line.find("#shader") != std::string::npos)
-			{
-				if (line.find("vertex") != std::string::npos)
-					type = ShaderType::VERTEX;
-				else if (line.find("fragment") != std::string::npos)
-					type = ShaderType::FRAGMENT;
-			}
-			else
-			{
-				ss[(int)type] << line << "\n";
-			}
-		}
-
-		return { ss[0].str(), ss[1].str() };
-	}
-
-	static unsigned int CompileShader(unsigned int type, const std::string& source)
-	{
-		unsigned int id = glCreateShader(type);
-		const char* src = source.c_str();
-
-		glShaderSource(id, 1, &src, nullptr);
-		glCompileShader(id);
-
-		//error handling
-		int result;
-		glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-
-		if (result == GL_FALSE)
-		{
-			int length;
-			glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-			char* message = (char*)alloca(length * sizeof(char));
-			glGetShaderInfoLog(id, length, &length, message);
-
-			std::cout << "Failed to compile" << (type == GL_VERTEX_SHADER ? "vertex" : "framgent") << " shader" << std::endl;
-			std::cout << message << std::endl;
-			glDeleteShader(id);
-			return 0;
-		}
-
-		return id;
-	}
-
-	static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
-	{
-		unsigned int program = glCreateProgram();
-
-		unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-		unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-		glAttachShader(program, vs);
-		glAttachShader(program, fs);
-
-		glLinkProgram(program);
-		glValidateProgram(program);
-
-		glDeleteShader(vs);
-		glDeleteShader(fs);
-
-		return program;
-	}
-}
-
 OpenGLRenderer::OpenGLRenderer()
 {
 	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
@@ -120,23 +30,22 @@ OpenGLRenderer::~OpenGLRenderer()
 
 void OpenGLRenderer::Present()
 {
-	std::unique_ptr<Mesh> mesh = MeshFactory::CreateQuad();
+	for (SceneObject* object : m_scene_objects)
+	{
+		Mesh* mesh = object->GetMesh();
+		Material* material = object->GetMaterial();
 
-	OpenGLVertexArray* va = new OpenGLVertexArray();
-	va->BindLayoutToBuffer(mesh->GetVertices(), mesh->GetVertexLayout());
+		float color[] = { 1.0f, 0.3f, 0.8f, 1.0f };
+		material->SetParameter("u_Color", color, sizeof(color));
+		material->Use();
 
-	mesh->GetIndices()->Bind();
+		VertexArray* va = VertexArray::Create(mesh->GetVertices(), mesh->GetVertexLayout());
+		mesh->GetIndices()->Bind();
 
-	std::unique_ptr<OpenGLVertexShader> vertex_shader = std::make_unique<OpenGLVertexShader>("data/shaders/opengl/vertexshader.glsl");
-	std::unique_ptr<OpenGLFragmentShader> fragment_shader = std::make_unique<OpenGLFragmentShader>("data/shaders/opengl/fragmentshader.glsl");
+		GLCALL(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0));
+	}
 
-	std::unique_ptr<OpenGLShaderProgram> program = std::make_unique<OpenGLShaderProgram>();
-	program->Create({ vertex_shader->GetId(), fragment_shader->GetId() });
-	program->Bind();
-
-	program->SetVector4Uniform("u_Color", 1.0f, 0.3f, 0.8f, 1.0f);
-
-	GLCALL(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0));
+	m_scene_objects.clear();
 }
 
 void OpenGLRenderer::ClearAllBuffers()
