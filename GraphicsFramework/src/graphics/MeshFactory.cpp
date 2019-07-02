3add89964b033b5043f95c05ac29d8f86e3258b4
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-03 01:35:39 +0200

    quad now holds vec3 as position

--- GraphicsFramework/src/graphics/MeshFactory.cpp
+++ GraphicsFramework/src/graphics/MeshFactory.cpp
@@ -9,12 +9,12 @@
 
 std::unique_ptr<Mesh> MeshFactory::CreateQuad()
 {
-	std::array<float, 8> positions = 
+	std::array<float, 16> positions = 
 	{
-		-0.5f, -0.5f, //0
-		 0.5f, -0.5f, //1
-		 0.5f,  0.5f, //2
-		-0.5f,  0.5f  //3
+		-0.5f, -0.5f, 0.0f,	//0
+		 0.5f, -0.5f, 0.0f,	//1
+		 0.5f,  0.5f, 0.0f,	//2
+		-0.5f,  0.5f, 0.0f	//3
 	};
 
 	std::array<int, 6> indices = 
@@ -26,7 +26,7 @@ std::unique_ptr<Mesh> MeshFactory::CreateQuad()
 	std::unique_ptr<VertexBuffer> vb = VertexBuffer::Create(sizeof(positions) , BufferUsage::STATIC, positions.data());
 	std::unique_ptr<IndexBuffer> ib = IndexBuffer::Create(6, BufferUsage::STATIC, indices.data());
 	std::unique_ptr<VertexLayout> layout = std::make_unique<VertexLayout>();
-	layout->Push<float>(VertexAttributeType::POSITION, 2);
+	layout->Push<float>(VertexAttributeType::POSITION, 3);
 
 	std::unique_ptr<Mesh> mesh = std::make_unique<Mesh>(vb, layout, ib);
 	return mesh;
