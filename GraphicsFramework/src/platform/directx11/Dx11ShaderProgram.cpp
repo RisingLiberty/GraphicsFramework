9dcd5809fcea88a619c2343ca53247fc1b07292b
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-03 04:06:25 +0200

    included virtual methods to dx11 shader program

--- GraphicsFramework/src/platform/directx11/Dx11ShaderProgram.cpp
+++ GraphicsFramework/src/platform/directx11/Dx11ShaderProgram.cpp
@@ -12,3 +12,78 @@ Dx11ShaderProgram::Dx11ShaderProgram(VertexShader* vs, FragmentShader* fs):
 Dx11ShaderProgram::~Dx11ShaderProgram()
 {
 }
+
+void Dx11ShaderProgram::Bind() const
+{
+
+}
+
+void Dx11ShaderProgram::Unbind() const
+{
+
+}
+
+void Dx11ShaderProgram::SetBoolUniform(const std::string & name, bool uniform)
+{
+}
+
+void Dx11ShaderProgram::SetUShortUniform(const std::string & name, unsigned short & uniform)
+{
+}
+
+void Dx11ShaderProgram::SetShortUniform(const std::string & name, short uniform)
+{
+}
+
+void Dx11ShaderProgram::SetIntUniform(const std::string & name, int uniform)
+{
+}
+
+void Dx11ShaderProgram::SetUIntUniform(const std::string & name, unsigned int & uniform)
+{
+}
+
+void Dx11ShaderProgram::SetFloatUniform(const std::string & name, float uniform)
+{
+}
+
+void Dx11ShaderProgram::SetDoubleUniform(const std::string & name, double uniform)
+{
+}
+
+void Dx11ShaderProgram::SetVec2Uniform(const std::string & name, float x, float y)
+{
+}
+
+void Dx11ShaderProgram::SetVec2Uniform(const std::string & name, float * values)
+{
+}
+
+void Dx11ShaderProgram::SetVec3Uniform(const std::string & name, float x, float y, float z)
+{
+}
+
+void Dx11ShaderProgram::SetVec3Uniform(const std::string & name, float * values)
+{
+}
+
+void Dx11ShaderProgram::SetVec4Uniform(const std::string & name, float x, float y, float z, float w)
+{
+}
+
+void Dx11ShaderProgram::SetVec4Uniform(const std::string & name, float * values)
+{
+}
+
+void Dx11ShaderProgram::SetMat3Uniform(const std::string & name, float * values)
+{
+}
+
+void Dx11ShaderProgram::SetMat4Uniform(const std::string & name, float * values)
+{
+}
+
+const std::vector<std::unique_ptr<ShaderUniform>>& Dx11ShaderProgram::GetUniforms() const
+{
+	return {};
+}
--- GraphicsFramework/src/platform/directx11/Dx11ShaderProgram.h
+++ GraphicsFramework/src/platform/directx11/Dx11ShaderProgram.h
@@ -11,5 +11,25 @@ public:
 	virtual void Bind() const override;
 	virtual void Unbind() const override;
 
+	virtual void SetBoolUniform(const std::string& name, bool uniform) override;
+	virtual void SetUShortUniform(const std::string& name, unsigned short& uniform) override;
+	virtual void SetShortUniform(const std::string& name, short uniform) override;
+	virtual void SetIntUniform(const std::string& name, int uniform) override;
+	virtual void SetUIntUniform(const std::string& name, unsigned int& uniform) override;
+	virtual void SetFloatUniform(const std::string& name, float uniform) override;
+	virtual void SetDoubleUniform(const std::string& name, double uniform) override;
+	virtual void SetVec2Uniform(const std::string& name, float x, float y) override;
+	virtual void SetVec2Uniform(const std::string& name, float* values) override;
+	virtual void SetVec3Uniform(const std::string& name, float x, float y, float z) override;
+	virtual void SetVec3Uniform(const std::string& name, float* values) override;
+	virtual void SetVec4Uniform(const std::string& name, float x, float y, float z, float w) override;
+	virtual void SetVec4Uniform(const std::string& name, float* values) override;
+	virtual void SetMat3Uniform(const std::string& name, float* values) override;
+	virtual void SetMat4Uniform(const std::string& name, float* values) override;
+
+	const std::vector<std::unique_ptr<ShaderUniform>>& GetUniforms() const override;
+private:
+
+	
 };
 
