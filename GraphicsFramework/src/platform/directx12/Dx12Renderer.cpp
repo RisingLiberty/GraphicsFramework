a3aefeb3465622b93c9855e272b4ddb2029d8c15
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-05 01:51:59 +0200

    dx12 renderer is no longer an abstract class

--- GraphicsFramework/src/platform/directx12/Dx12Renderer.cpp
+++ GraphicsFramework/src/platform/directx12/Dx12Renderer.cpp
@@ -11,3 +11,27 @@ Dx12Renderer::~Dx12Renderer()
 {
 
 }
+
+void Dx12Renderer::Present()
+{
+}
+
+void Dx12Renderer::ClearAllBuffers()
+{
+}
+
+void Dx12Renderer::ClearColorBuffer()
+{
+}
+
+void Dx12Renderer::ClearDepthStencilBuffer()
+{
+}
+
+void Dx12Renderer::ClearDepthBuffer()
+{
+}
+
+void Dx12Renderer::ClearStencilBuffer()
+{
+}
--- GraphicsFramework/src/platform/directx12/Dx12Renderer.h
+++ GraphicsFramework/src/platform/directx12/Dx12Renderer.h
@@ -4,9 +4,15 @@
 
 class Dx12Renderer : public Renderer
 {
-public: 
+public:
 	Dx12Renderer();
 	virtual ~Dx12Renderer();
 
+	virtual void Present() override;
+	virtual void ClearAllBuffers() override;
+	virtual void ClearColorBuffer() override;
+	virtual void ClearDepthStencilBuffer() override;
+	virtual void ClearDepthBuffer() override;
+	virtual void ClearStencilBuffer() override;
 
 };
