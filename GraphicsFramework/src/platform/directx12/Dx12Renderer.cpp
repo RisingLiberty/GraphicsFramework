9063d524da15ba5992734403f928dcac1325873b
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-04 15:42:55 +0200

    dx12 renderer created

---
+++ GraphicsFramework/src/platform/directx12/Dx12Renderer.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+
+#include "Dx12Renderer.h"
+
+Dx12Renderer::Dx12Renderer()
+{
+
+}
+
+Dx12Renderer::~Dx12Renderer()
+{
+
+}
---
+++ GraphicsFramework/src/platform/directx12/Dx12Renderer.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "graphics/Renderer.h"
+
+class Dx12Renderer : public Renderer
+{
+public: 
+	Dx12Renderer();
+	virtual ~Dx12Renderer();
+
+
+};
