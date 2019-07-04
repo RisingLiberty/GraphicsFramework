36354563417fe1dd0b70fb185ef78842d92a9fe3
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-05 01:24:16 +0200

    Create default buffer helper function added

--- GraphicsFramework/src/platform/directx12/Dx12HelperMethods.cpp
+++ GraphicsFramework/src/platform/directx12/Dx12HelperMethods.cpp
@@ -62,3 +62,40 @@ ID3D12GraphicsCommandList* GetDx12CommandList()
 {
 	return GetDx12Context()->GetCommandList();
 }
+
+ComPtr<ID3D12Resource> CreateDefaultBuffer(const void* data, unsigned int size, ComPtr<ID3D12Resource> uploadBuffer)
+{
+	ID3D12Device* device = GetDx12Device();
+	ID3D12GraphicsCommandList* command_list = GetDx12CommandList();
+
+	ComPtr<ID3D12Resource> default_buffer;
+
+	DXCALL(device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(size),
+		D3D12_RESOURCE_STATE_COMMON,
+		nullptr,
+		IID_PPV_ARGS(default_buffer.GetAddressOf())));
+
+	DXCALL(device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+		D3D12_HEAP_FLAG_NONE,
+		&CD3DX12_RESOURCE_DESC::Buffer(size),
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(uploadBuffer.GetAddressOf())));
+
+	D3D12_SUBRESOURCE_DATA sub_resource_data = {};
+	sub_resource_data.pData = data;
+	sub_resource_data.RowPitch = size;
+	sub_resource_data.SlicePitch = sub_resource_data.RowPitch;
+
+	command_list->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(default_buffer.Get(),
+		D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_COPY_DEST));
+	UpdateSubresources<1>(command_list, default_buffer.Get(), uploadBuffer.Get(), 0, 0, 1, &sub_resource_data);
+	command_list->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(default_buffer.Get(),
+		D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_GENERIC_READ));
+
+	return default_buffer;
+}
