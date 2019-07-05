391310e3e6f8dfe31e740f238f4206cf6f067219
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-05 21:57:39 +0200

    context now created dx12 context

--- GraphicsFramework/src/graphics/Context.cpp
+++ GraphicsFramework/src/graphics/Context.cpp
@@ -6,6 +6,7 @@
 #include "platform/opengl/OpenGLContext.h"
 #include "platform/directx11/Dx11Context.h"
 #include "platform/directx12/Dx12Context.h"
+#include "platform/directx12/Dx12TestContext.h"
 
 #include "controllers/ShaderController.h"
 #include "controllers/VertexArrayController.h"
@@ -27,7 +28,7 @@ void Context::Create(API api, Window* window)
 	switch (api)
 	{
 	case Context::API::DIRECTX11: s_current = std::make_unique<Dx11Context>(window); break;
-	case Context::API::DIRECTX12: s_current = std::make_unique<Dx12Context>(window); break;
+	case Context::API::DIRECTX12: s_current = std::make_unique<Dx12TestContext>(window); break;
 	case Context::API::OPENGL: s_current = std::make_unique<OpenGLContext>(window); break;
 	case Context::API::VULKAN:
 		break;
