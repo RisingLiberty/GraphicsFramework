0d698fbe365fa2370d5e9af763c60347b168397a
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-06 22:46:38 +0200

    vk context initialization up till gpu picking

--- GraphicsFramework/src/platform/vulkan/VkContext.cpp
+++ GraphicsFramework/src/platform/vulkan/VkContext.cpp
@@ -1,9 +1,40 @@
 #include "stdafx.h"
 
 #include "VkContext.h"
+#include "VkHelperMethods.h"
+
+#include "graphics/Window.h"
+
+namespace
+{
+	void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT callback, const VkAllocationCallbacks* pAllocator)
+	{
+		auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+		if (func != nullptr)
+			func(instance, callback, pAllocator);
+	}
+
+	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pCallback)
+	{
+		auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+
+		if (func != nullptr)
+			return func(instance, pCreateInfo, pAllocator, pCallback);
+		else
+			return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+
+	void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT callback, const VkAllocationCallbacks* pAllocator)
+	{
+		auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+		if (func != nullptr)
+			func(instance, callback, pAllocator);
+	}
+}
 
 VkContext::VkContext(Window* window)
 {
+	this->CreateInstance(window);
 
 }
 
@@ -14,6 +45,100 @@ VkContext::~VkContext()
 
 void VkContext::Initialize()
 {
+	this->ShowExtentions();
+	this->SetupDebugCallback();
+	this->PickPhysicalDevice();
+	this->CreateLogicalDevice();
+	this->CreateSwapchain();
+	this->CreateImageViews();
+	this->CreateRenderPass();
+	this->CreateDescriptorSetLayout();
+	this->CreateGraphicsPipeline();
+	this->CreateCommandPool();
+	this->CreateColorResources();
+	this->CreateDepthResources();
+	this->CreateFrameBuffers();
+	this->CreateTextureImage();
+	this->CreateTextureImageView();
+	this->CreateTextureSampler();
+	this->LoadModel();
+	this->CreateVertexBuffer();
+	this->CreateIndexBuffer();
+	this->CreateUniformBuffer();
+	this->CreateDescriptorPool();
+	this->CreateDescriptorSets();
+	this->CreateCommandBuffers();
+	this->CreateSyncObjects();
+}
+
+void VkContext::Cleanup()
+{
+	//Clean vulkan
+	this->CleanupSwapchain();
+
+	vkDestroyImageView(m_device, m_color_image_view, nullptr);
+	vkDestroyImage(m_device, m_color_image, nullptr);
+	vkFreeMemory(m_device, m_color_image_memory, nullptr);
+
+	vkDestroyImage(m_device, m_depth_image, nullptr);
+	vkDestroyImageView(m_device, m_depth_image_view, nullptr);
+	vkFreeMemory(m_device, m_depth_image_memory, nullptr);
+
+	vkDestroySampler(m_device, m_texture_sampler, nullptr);
+
+	vkDestroyImageView(m_device, m_texture_image_view, nullptr);
+
+	vkDestroyImage(m_device, m_texture_image, nullptr);
+	vkFreeMemory(m_device, m_texture_image_memory, nullptr);
+
+	vkDestroyDescriptorPool(m_device, m_descriptor_pool, nullptr);
+
+	vkDestroyDescriptorSetLayout(m_device, m_descriptor_set_layout, nullptr);
+
+	for (size_t i = 0; i < m_swapchain_images.size(); ++i)
+	{
+		vkDestroyBuffer(m_device, m_uniform_buffers[i], nullptr);
+		vkFreeMemory(m_device, m_uniform_buffers_memory[i], nullptr);
+	}
+
+	vkDestroyBuffer(m_device, m_index_buffer, nullptr);
+	vkFreeMemory(m_device, m_index_buffer_memory, nullptr);
+
+	vkDestroyBuffer(m_device, m_vertex_buffer, nullptr);
+	vkFreeMemory(m_device, m_vertex_buffer_memory, nullptr);
+
+	for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; ++i)
+	{
+		vkDestroySemaphore(m_device, m_render_finished_semaphores[i], nullptr);
+		vkDestroySemaphore(m_device, m_image_available_semaphores[i], nullptr);
+		vkDestroyFence(m_device, m_in_flight_fences[i], nullptr);
+	}
+
+	vkDestroyCommandPool(m_device, m_command_pool, nullptr);
+	vkDestroyDevice(m_device, nullptr);
+
+	if (m_enable_validation_layers)
+		DestroyDebugUtilsMessengerEXT(m_instance, m_debug_callback, nullptr);
+
+	vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
+
+	vkDestroyInstance(m_instance, nullptr);
+}
+
+void VkContext::CleanupSwapchain()
+{
+	for (size_t i = 0; i < m_swapchain_frame_buffers.size(); ++i)
+		vkDestroyFramebuffer(m_device, m_swapchain_frame_buffers[i], nullptr);
+
+	vkFreeCommandBuffers(m_device, m_command_pool, static_cast<uint32_t>(m_command_buffers.size()), m_command_buffers.data());
+	vkDestroyPipeline(m_device, m_graphics_pipeline, nullptr);
+	vkDestroyPipelineLayout(m_device, m_pipeline_layout, nullptr);
+	vkDestroyRenderPass(m_device, m_render_pass, nullptr);
+
+	for (size_t i = 0; i < m_swapchain_image_views.size(); ++i)
+		vkDestroyImageView(m_device, m_swapchain_image_views[i], nullptr);
+
+	vkDestroySwapchainKHR(m_device, m_swapchain, nullptr);
 }
 
 void VkContext::Present()
@@ -23,4 +148,228 @@ void VkContext::Present()
 Context::API VkContext::GetApiType() const
 {
 	return API::VULKAN;
+}
+
+void VkContext::CreateInstance(Window* window)
+{
+	if (m_enable_validation_layers && !this->CheckValidationLayerSupport())
+		ThrowException("vilidation layers requested, but not available!");
+
+	VkApplicationInfo app_info = {};
+
+	app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	app_info.pApplicationName = window->GetTitle().c_str();
+	app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	app_info.pEngineName = "No Engine";
+	app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	app_info.apiVersion = VK_API_VERSION_1_0;
+
+	VkInstanceCreateInfo create_info = {};
+	create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	create_info.pApplicationInfo = &app_info;
+	
+	std::vector<const char*> extentions = GetRequiredExtentions();
+	create_info.enabledExtensionCount = static_cast<uint32_t>(extentions.size());
+	create_info.ppEnabledExtensionNames = extentions.data();
+	
+	if (m_enable_validation_layers)
+	{
+		create_info.enabledLayerCount = static_cast<uint32_t>(m_validation_layers.size());
+		create_info.ppEnabledLayerNames = m_validation_layers.data();
+	}
+	else
+		create_info.enabledExtensionCount = 0;
+
+	VKCALL(vkCreateInstance(&create_info, nullptr, &m_instance));
+}
+
+void VkContext::ShowExtentions()
+{
+	uint32_t extension_count = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+
+	std::vector<VkExtensionProperties> extensions(extension_count);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, extensions.data());
+
+	spdlog::info("available extensions:");
+
+	for (const VkExtensionProperties& extension : extensions)
+		spdlog::info("\t{}", extension.extensionName);
+}
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
+	VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
+	VkDebugUtilsMessageTypeFlagsEXT messageType, 
+	const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, 
+	void* pUserData)
+{
+	if (messageSeverity > -VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT)
+		spdlog::error("Fatal Vulkan error!");
+
+	spdlog::error("validation layer: {}", pCallbackData->pMessage);
+
+	return VK_FALSE;
+}
+
+void VkContext::SetupDebugCallback()
+{
+	if (!m_enable_validation_layers)
+		return;
+
+	VkDebugUtilsMessengerCreateInfoEXT create_info = {};
+	create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT;
+
+	create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+
+	create_info.pfnUserCallback = DebugCallback;
+	create_info.pUserData = nullptr; // Optional
+
+	VKCALL(CreateDebugUtilsMessengerEXT(m_instance, &create_info, nullptr, &m_debug_callback));
+}
+
+void VkContext::PickPhysicalDevice()
+{
+}
+
+void VkContext::CreateLogicalDevice()
+{
+}
+
+void VkContext::CreateSwapchain()
+{
+}
+
+void VkContext::CreateImageViews()
+{
+}
+
+void VkContext::CreateRenderPass()
+{
+}
+
+void VkContext::CreateDescriptorSetLayout()
+{
+}
+
+void VkContext::CreateGraphicsPipeline()
+{
+}
+
+void VkContext::CreateCommandPool()
+{
+}
+
+void VkContext::CreateColorResources()
+{
+}
+
+void VkContext::CreateDepthResources()
+{
+}
+
+void VkContext::CreateFrameBuffers()
+{
+}
+
+void VkContext::CreateTextureImage()
+{
+}
+
+void VkContext::CreateTextureImageView()
+{
+}
+
+void VkContext::CreateTextureSampler()
+{
+}
+
+void VkContext::LoadModel()
+{
+}
+
+void VkContext::CreateVertexBuffer()
+{
+}
+
+void VkContext::CreateIndexBuffer()
+{
+}
+
+void VkContext::CreateUniformBuffer()
+{
+}
+
+void VkContext::CreateDescriptorPool()
+{
+}
+
+void VkContext::CreateDescriptorSets()
+{
+}
+
+void VkContext::CreateCommandBuffers()
+{
+}
+
+void VkContext::CreateSyncObjects()
+{
+}
+
+bool VkContext::CheckValidationLayerSupport() const
+{
+	uint32_t layer_count = 0;
+	vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
+
+	std::vector<VkLayerProperties> availableLayers(layer_count);
+	vkEnumerateInstanceLayerProperties(&layer_count, availableLayers.data());
+
+	for (const char* layerName : m_validation_layers)
+	{
+		bool layer_found = false;
+
+		for (const VkLayerProperties& layer_props : availableLayers)
+		{
+			if (strcmp(layerName, layer_props.layerName) == 0)
+			{
+				layer_found = true;
+				break;
+			}
+		}
+
+		if (!layer_found)
+			return false;
+	}
+
+	return true;
+}
+
+std::vector<const char*> VkContext::GetRequiredExtentions() const
+{
+	// these are normally the extentsions returned by glfw
+	// if surface is enabled (which is true in this case)
+	// and we're developing for windows
+
+	return 
+	{
+		strdup("VK_KHR_surface"),
+		strdup("VK_KHR_win32_surface")
+	};
+
+	//TODO: get real extentions from glfw
+	//uint32_t glfwExtensionCount = 0;
+	//const char** glfwExtensions;
+	//glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	//std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+	//if (m_EnableValidationLayers)
+	//	extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+
+	//return extensions;
 }
--- GraphicsFramework/src/platform/vulkan/VkContext.h
+++ GraphicsFramework/src/platform/vulkan/VkContext.h
@@ -4,6 +4,116 @@
 
 class Window;
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
+#include <glm/gtc/matrix_transform.hpp>
+
+struct Vertex
+{
+	glm::vec3 Position;
+	glm::vec3 Color;
+	glm::vec2 TexCoord;
+
+	static VkVertexInputBindingDescription GetBindingDescription()
+	{
+		//A vertex binding describes at which rate to load data from memory throught the vertices.
+		//It specifies the number of bytes between data entries and wheter to move to the next data entry
+		//after each vertex or after each instance.
+		VkVertexInputBindingDescription bindingDescription = {};
+
+		//all of our per-vertex data is packed together in one array, so we're only going to have one binding.
+		//the binding paramter specifies the index of the binding in the array of bindings.
+		//The stride parameters specifies the number of bytes from one entry to the next
+		//the inputRate parameter can have one of the following values:
+		//VK_VERTEX_INPUT_RATE_VERTEX: move to the next data entry after each vertex
+		//VK_VERTEX_INPUT_RATE_INSTANCE: move to the next data entry after each instance.
+		//we're not going to use instanced rendering, so we'll stick to per-vertex data.
+		bindingDescription.binding = 0;
+		bindingDescription.stride = sizeof(Vertex);
+		bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+		return bindingDescription;
+	}
+
+	static std::array<VkVertexInputAttributeDescription, 3> GetAttributeDescriptions()
+	{
+		//The second structure that describes how to handle vertex input is VkVertexInputAttributeDescription.
+		std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions = {};
+
+		//An attribute description struct describes how to extract a vertex attribute from a chuknk of vertex data.
+		//originating from a binding description.
+		//We have 2 attributes, position and color, so we need 2 attribute description structs
+
+		//POSITION
+		//------------------
+
+		//Tells Vulkan from which binding the per-vertex data comes
+		attributeDescriptions[0].binding = 0;
+		//The location parameter references the location directive of the input in the vertex shader
+		//The input in the vertex shader with location 0 is the position, which has 2 32-bit float components
+		attributeDescriptions[0].location = 0;
+		//The format parameter describes the type of data for the attribute.
+		//A bit confusingly, the formata are specified using the same enumeration as color formats.
+		//The following shader types and formats are commonly used together:
+		//float: VK_FORMAT_R32_SFLOAT
+		//vec2: VK_FORMAT_R32G32_SFLOAT
+		//vec3: VK_FORMAT_R32G32B32_SFLOAT
+		//vec4: VK_FORMAT_R32G32B32A32_SFLOAT
+		//The color type (SFLOAT_, UINT, SINT) and bit width should also match the type of the shader input.
+		//Examples:
+		//ivec2: VK_FORMAT_R32G32_SINT, a 2-component vector of 32-bit signed integers
+		//uvec4: VK_FORMAT_R32G32B32A32, a 4-component vector of 32-bit unsigned integers
+		//double: VK_FORMAT_R64_SFLOAT, a double-precision (64-bit) float
+		attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+		//specifies the number of bytes since the start of the per-vertex data to read from.
+		//The bniding is loading one Vertex at a time and the position attribute(Position) i at an offset of 0 bytes
+		//from the beginning of this struct. This is automatically calculated using the offsetof macro.
+		attributeDescriptions[0].offset = offsetof(Vertex, Position);
+
+		//COLOR
+		//-----------------
+		attributeDescriptions[1].binding = 0;
+		attributeDescriptions[1].location = 1;
+		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+		attributeDescriptions[1].offset = offsetof(Vertex, Color);
+
+		//TEXCOORD
+		//-----------------
+		attributeDescriptions[2].binding = 0;
+		attributeDescriptions[2].location = 2;
+		attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+		attributeDescriptions[2].offset = offsetof(Vertex, TexCoord);
+
+		return attributeDescriptions;
+	}
+
+	bool operator==(const Vertex& other) const
+	{
+		return Position == other.Position && Color == other.Color && TexCoord == other.TexCoord;
+	}
+
+};
+
+namespace std
+{
+	template<> struct hash<Vertex>
+	{
+		size_t operator()(Vertex const& vertex) const
+		{
+			return ((hash<glm::vec3>()(vertex.Position) ^
+				(hash<glm::vec3>()(vertex.Color) << 1)) >> 1) ^
+				(hash<glm::vec2>()(vertex.TexCoord) << 1);
+		}
+	};
+}
+
+
+
 class VkContext : public Context
 {
 public:
@@ -13,4 +123,108 @@ public:
 	virtual void Initialize();
 	virtual void Present();
 	virtual API GetApiType() const;
+
+private:
+	void CreateInstance(Window* window);
+	void ShowExtentions();
+	void SetupDebugCallback();
+	void PickPhysicalDevice();
+	void CreateLogicalDevice();
+	void CreateSwapchain();
+	void CreateImageViews();
+	void CreateRenderPass();
+	void CreateDescriptorSetLayout();
+	void CreateGraphicsPipeline();
+	void CreateCommandPool();
+	void CreateColorResources();
+	void CreateDepthResources();
+	void CreateFrameBuffers();
+	void CreateTextureImage();
+	void CreateTextureImageView();
+	void CreateTextureSampler();
+	void LoadModel();
+	void CreateVertexBuffer();
+	void CreateIndexBuffer();
+	void CreateUniformBuffer();
+	void CreateDescriptorPool();
+	void CreateDescriptorSets();
+	void CreateCommandBuffers();
+	void CreateSyncObjects();
+
+	void Cleanup();
+	void CleanupSwapchain();
+
+	bool CheckValidationLayerSupport() const;
+	std::vector<const char*> GetRequiredExtentions() const;
+private:
+	VkInstance m_instance;
+	VkDebugUtilsMessengerEXT m_debug_callback;
+	VkPhysicalDevice* m_physical_device;
+	VkSurfaceKHR m_surface;
+	VkDevice m_device;
+	VkQueue m_graphics_queue;
+	VkQueue m_present_queue;
+	VkSwapchainKHR m_swapchain;
+	std::vector<VkImage> m_swapchain_images;
+	VkFormat m_swapchain_image_format;
+	VkExtent2D m_swapchain_extent;
+	std::vector<VkImageView> m_swapchain_image_views;
+	VkRenderPass m_render_pass;
+	VkPipelineLayout m_pipeline_layout;
+	VkPipeline m_graphics_pipeline;
+	std::vector<VkFramebuffer> m_swapchain_frame_buffers;
+	VkCommandPool m_command_pool;
+	std::vector<VkCommandBuffer> m_command_buffers;
+	std::vector<VkSemaphore> m_image_available_semaphores;
+	std::vector<VkSemaphore> m_render_finished_semaphores;
+	std::vector<VkFence> m_in_flight_fences;
+	size_t m_current_frame;
+	bool m_is_frame_buffer_resized;
+	VkBuffer m_vertex_buffer;
+	VkDeviceMemory m_vertex_buffer_memory;
+	VkBuffer m_index_buffer;
+	VkDeviceMemory m_index_buffer_memory;
+	VkDescriptorSetLayout m_descriptor_set_layout;
+	std::vector<VkBuffer> m_uniform_buffers;
+	std::vector<VkDeviceMemory> m_uniform_buffers_memory;
+	VkDescriptorPool m_descriptor_pool;
+	std::vector<VkDescriptorSet> m_descriptor_sets;
+	uint32_t m_mip_levels;
+	VkImage m_texture_image;
+	VkDeviceMemory m_texture_image_memory;
+	VkImageView m_texture_image_view;
+	VkSampler m_texture_sampler;
+	VkImage m_depth_image;
+	VkDeviceMemory m_depth_image_memory;
+	VkImageView m_depth_image_view;
+	VkSampleCountFlagBits m_msaa_samples;
+	VkImage m_color_image;
+	VkDeviceMemory m_color_image_memory;
+	VkImageView m_color_image_view;
+	const std::vector<const char*> m_validation_layers = { "VK_LYER_LUNARG_standard_validation" };
+	const std::vector<const char*> m_device_extentions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
+	const int MAX_FRAMES_IN_FLIGHT = 2;
+
+	std::vector<Vertex> m_Vertices =
+	{
+		//		Position				Color			TexCoord
+		//-----------------------------------------------------------
+		{{-0.5f, -0.5f,  0.0f},  {1.0f, 0.0f, 0.0f},  {1.0f, 0.0f}},
+		{{ 0.5f, -0.5f,  0.0f},  {0.0f, 1.0f, 0.0f},  {0.0f, 0.0f}},
+		{{ 0.5f,  0.5f,  0.0f},  {0.0f, 0.0f, 1.0f},  {0.0f, 1.0f}},
+		{{-0.5f,  0.5f,  0.0f},  {1.0f, 1.0f, 1.0f},  {1.0f, 1.0f}}
+	};
+	
+	std::vector<uint32_t> m_Indices =
+	{
+		//First plane
+		0,1,2, //top right
+		2,3,0,  //bottom left
+	};
+	
+	bool m_enable_validation_layers = true;
+
+	const std::string MODEL_PATH = "data/meshes/chalet.obj";
+	const std::string TEXTURE_PATH = "data/textures/chalet.obj";
 };
