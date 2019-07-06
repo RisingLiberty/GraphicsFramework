e100c0f612a87a8a10d83783ef2a5a9edb3453ca
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-06 18:18:44 +0200

    deleted vertex file

--- GraphicsFramework/src/graphics/Vertex.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-
-#include "Vertex.h"
-
-bool Vertex::operator==(const Vertex& other) const
-{
-	return Position == other.Position && Color == other.Color && TexCoord == other.TexCoord;
-}
