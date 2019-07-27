ced5e3ae00c7ed769a5116fa8e86851745210d0d
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-27 16:30:18 +0200

    fix build

--- GraphicsFramework/src/graphics/MeshFactory.cpp
+++ GraphicsFramework/src/graphics/MeshFactory.cpp
@@ -9,7 +9,7 @@
 
 std::unique_ptr<Mesh> MeshFactory::CreateQuad()
 {
-	std::array<float, 16> positions = 
+	std::array<float, 12> positions = 
 	{
 		-0.5f, -0.5f, 0.0f,	//0
 		 0.5f, -0.5f, 0.0f,	//1
@@ -17,17 +17,18 @@ std::unique_ptr<Mesh> MeshFactory::CreateQuad()
 		-0.5f,  0.5f, 0.0f	//3
 	};
 
-	std::array<int, 6> indices = 
+	std::array<char, 6> indices = 
 	{
 		0,1,2,
 		2,3,0
 	};
 
-	std::unique_ptr<VertexBuffer> vb = VertexBuffer::Create(sizeof(positions) , BufferUsage::STATIC, positions.data());
-	std::unique_ptr<IndexBuffer> ib = IndexBuffer::Create(indices.size(), BufferUsage::STATIC, indices.data());
-	std::unique_ptr<VertexLayout> layout = VertexLayout::Create();
+	VertexBuffer* vb = VertexBuffer::Create(sizeof(positions) , BufferUsage::STATIC, positions.data());
+	IndexBuffer* ib = IndexBuffer::Create(indices.size(), Format(EFormat::R8_UINT), Topology(ETopology::TRIANGLELIST), BufferUsage::STATIC, indices.data());
+	VertexLayout* layout = VertexLayout::Create();
 	layout->Push<float>(VertexAttributeType::POSITION, 3);
 
 	std::unique_ptr<Mesh> mesh = std::make_unique<Mesh>(vb, layout, ib);
+
 	return mesh;
 }
