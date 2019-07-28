0f9a771e9d6c6a550d9ce401b74e7b8c01b2f94e
Nick De Breuck <nick.de.breuck@outlook.com>
2019-07-28 02:43:09 +0200

    Context now clears bound data before rendering

--- GraphicsFramework/src/platform/win64/Win64Application.cpp
+++ GraphicsFramework/src/platform/win64/Win64Application.cpp
@@ -30,7 +30,7 @@ Win64Application::Win64Application(AreFramesCapped areFramesCapped):
 {
 	m_window = std::make_unique<Win64Window>(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 	m_window->SetEventCallback(std::bind(&Application::OnEvent, this, std::placeholders::_1));
-	Context::Create(Context::API::VULKAN, m_window.get());
+	Context::Create(Context::API::DIRECTX11, m_window.get());
 }
 
 Win64Application::~Win64Application() = default;
@@ -99,14 +99,14 @@ void Win64Application::Update(float dTime)
 
 void Win64Application::Draw()
 {
-	Context::GetCurrent()->Start();
+	Context::GetCurrent()->Begin();
+	Context::GetCurrent()->Clear();
 	Context::GetCurrent()->GetRenderer()->Begin();
-	Context::GetCurrent()->GetRenderer()->ClearAllBuffers();
 	m_scene_controller->Draw();
 	Context::GetCurrent()->GetRenderer()->Draw();
 	Context::GetCurrent()->GetRenderer()->RenderImgui();
 	Context::GetCurrent()->GetRenderer()->End();
-	Context::GetCurrent()->Finish();
+	Context::GetCurrent()->End();
 }
 
 void Win64Application::OnEvent(const Event& event)
